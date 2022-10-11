ac68c9767f8b417486404c377bd1dcbb38683a3e
Jason Williams <100530581+imjaybear@users.noreply.github.com>
2022-10-11 18:23:07 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link, Switch, Route} from "react-router-dom";
+
+function Home() {
+  return <h1>Welcome to the home page</h1>
+}
+function About() {
+  return <h1>You are on the about page</h1>
+}
+function Contact() {
+  return <h1>Please feel free to email us</h1>
+}
+function NoMatch() {
+  return <h1>404 Not Found</h1>
+}
+
+function App () {
+  return (
+    // No need to add <Router>, it has been added to ./index.js
+    <div className="App">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <NoMatch />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
